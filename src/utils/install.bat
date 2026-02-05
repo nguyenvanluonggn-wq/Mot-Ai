d2d34dd73b7c897e9df5150ff33e41e46e218231
nguyenvanluonggn-wq <nguyenvanluong.gn@gmail.com>
2026-02-05 11:08:08 +0700

    Update install.bat

--- src/utils/install.bat
+++ src/utils/install.bat
@@ -1,1 +1,6 @@
-
+echo [2/4] Tạo thư mục cần thiết...
+if not exist "assets" mkdir assets
+if not exist "output" mkdir output
+if not exist "logs" mkdir logs
+if not exist "models" mkdir models
+if not exist "src\utils" mkdir src\utils  ← THÊM DÒNG NÀY
